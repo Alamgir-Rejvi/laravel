88d798971bc5ac313b0e4613e589e9d69ed8aa6c
AlamgirRejvi <alamgirrejvi65@gmail.com>
2025-10-31 14:06:48 +0600

    new

--- food_delivery/routes/web.php
+++ food_delivery/routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/post/{id}',function(string $id){
+//     return "<h1> Post ID : " .$id. "</h1>";
+// });
+
+Route::get('/post/{id?}',function(string $id = null){
+    if($id){
+        return "<h1> Post ID : " .$id. "</h1>";
+    }else{
+        return "<h1>ID is not Found</h1>";
+    };
+    
+});
